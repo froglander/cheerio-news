34ff3fa0edf5693cce79b10aef1ccefe84d8b9be
Kristi Heredia <froglander@dukworld.net>
2016-11-08 15:53:03 -0500

    Small changes

--- public/app.js
+++ public/app.js
@@ -15,18 +15,18 @@ $(document).on('click', 'p', function(){
     .done(function( data ) {
       console.log(data);
       // the title of the article
-      $('#notes').append('<h2>' + data.title + '</h2>'); 
+      $('#savednotes').append('<h2>' + data.title + '</h2>');
       // an input to enter a new title
-      $('#notes').append('<input id="titleinput" name="title" >'); 
+      // $('#notes').append('<input id="titleinput" name="title" >');
       // a textarea to add a new note body
-      $('#notes').append('<textarea id="bodyinput" name="body"></textarea>'); 
+      $('#savednotes').append('<textarea id="bodyinput" name="body"></textarea>');
       // a button to submit a new note, with the id of the article saved to it
-      $('#notes').append('<button data-id="' + data._id + '" id="savenote">Save Note</button>');
+      $('#savednotes').append('<button data-id="' + data._id + '" id="savenote">Save Note</button>');
 
       // if there's a note in the article
       if(data.note){
         // place the title of the note in the title input
-        $('#titleinput').val(data.note.title);
+        // $('#titleinput').val(data.note.title);
         // place the body of the note in the body textarea
         $('#bodyinput').val(data.note.body);
       }
@@ -43,7 +43,7 @@ $(document).on('click', '#savenote', function(){
     method: "POST",
     url: "/articles/" + thisId,
     data: {
-      title: $('#titleinput').val(), // value taken from title input
+      // title: $('#titleinput').val(), // value taken from title input
       body: $('#bodyinput').val() // value taken from note textarea
     }
   })
