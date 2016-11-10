2b5954e3978debd8123dd1ba42a27ce6dcceee71
Kristi Heredia <froglander@dukworld.net>
2016-11-10 17:03:32 -0500

    Update appearance

--- public/app.js
+++ public/app.js
@@ -21,20 +21,14 @@ $(document).on('click', 'p', function () {
             console.log(data);
             // the title of the article
             $headline.append('<h2>' + data.title + '</h2>');
-            // an input to enter a new title
-            // $('#notes').append('<input id="titleinput" name="title" >');
-            // a textarea to add a new note body
-            // $('#savednotes').append('<textarea id="bodyinput" name="body"></textarea>');
             $newNote.append('<textarea id="newbodyinput" name="body"></textarea>');
             $savedNotes.append('<textarea id="savedbodyinput" name="body"></textarea>');
+
             // a button to submit a new note, with the id of the article saved to it
-            // $('#savednotes').append('<button data-id="' + data._id + '" id="savenote">Save Note</button>');
             $newNote.append('<button data-id="' + data._id + '" id="savenote">Save Note</button>');
 
             // if there's a note in the article
             if (data.note) {
-                // place the title of the note in the title input
-                // $('#titleinput').val(data.note.title);
                 // place the body of the note in the body textarea
                 $('#savedbodyinput').val(data.note.body);
             }
@@ -46,13 +40,15 @@ $(document).on('click', '#savenote', function () {
     // grab the id associated with the article from the submit button
     var thisId = $(this).attr('data-id');
 
+    var noteText =  $('#newbodyinput').val();
     // run a POST request to change the note, using what's entered in the inputs
     $.ajax({
+
         method: "POST",
         url: "/articles/" + thisId,
         data: {
             // title: $('#titleinput').val(), // value taken from title input
-            body: $('#newbodyinput').val() // value taken from note textarea
+            body: noteText // value taken from note textarea
         }
     })
     // with that done
@@ -60,7 +56,8 @@ $(document).on('click', '#savenote', function () {
             // log the response
             console.log(data);
             // empty the notes section
-            $('#savednotes').empty();
+            //$('#savednotes').empty();
+            $('#savednotes').val(noteText)
 
         });
 
