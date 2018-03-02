424e20409f651aeddf3394238d83bcc72ad5db20
Kristi Heredia <froglander@dukworld.net>
2018-03-01 21:19:46 -0500

    Fix mongo link

--- server.js
+++ server.js
@@ -19,9 +19,14 @@ app.use(express.static('public'));
 
 // Set up the mongoose mongodb connection
 // Local for dev purposes
-// mongoose.connect("mongodb://localhost/cheerio-news");
+var localMongo = "mongodb://localhost/cheerio-news";
 // Heroku version
-mongoose.connect("mongodb://heroku_n5d1ttd3:4oiehtmqju59e1aosgv8du9fkb@ds041160.mlab.com:41160/heroku_n5d1ttd3");
+if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    mongoose.connect(localMongo);
+}
+
 var db = mongoose.connection;
 
 // Show any mongoose errors
